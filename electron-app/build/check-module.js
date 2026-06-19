dee917b0298d7ebc094b720e5391d7669604c93a
Luiz Fernando <lf7artes@gmail.com>
2026-06-19 10:01:23 -0300

    cleanup: remove temp build scripts, uninstall png-to-ico

--- electron-app/build/check-module.js
+++
@@ -1,8 +0,0 @@
-const mod = require('png-to-ico');
-console.log('type:', typeof mod);
-console.log('keys:', Object.keys(mod).join(', '));
-if (typeof mod === 'function') {
-  console.log('  (it is a function)');
-} else if (typeof mod === 'object' && mod.default) {
-  console.log('  default export type:', typeof mod.default);
-}
